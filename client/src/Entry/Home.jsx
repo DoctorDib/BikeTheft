2ec77ce6ad612eecf6fc816d9740666e69f555db
Jake Barter <midg3tjake@live.co.uk>
2020-09-23 21:48:11 +0100

    Router

--- client/src/Entry/Home.jsx
+++ client/src/Entry/Home.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import mainTheme from 'templates/theme';
-import Indexer from 'Pages/Home';
+
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import AppliedRoute from 'Components/Routing/Applied';
+
+import HomePage from 'Pages/Home';
+import BikePage from 'Pages/Bike';
 
 import { ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
     return (
-        <ThemeProvider theme={ mainTheme }>
-            <Indexer />
-        </ThemeProvider>
+        <Router>
+            <ThemeProvider theme={ mainTheme }>
+                <Switch>
+                    <AppliedRoute appProps={{}} path={'/'} component={HomePage}/>
+                    <AppliedRoute appProps={{}} path={'/bike'} component={BikePage}/>
+                    <AppliedRoute appProps={{}} path={'*'} component={HomePage}/>
+                </Switch>
+            </ThemeProvider>
+        </Router>
     );
 }
 
